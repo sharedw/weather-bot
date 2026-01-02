780ad0b6ae9d77f324299340b63bce598c80dab6
sharedw <sharedwestover@gmail.com>
2026-01-02 17:12:01 -0600

    fix try block

--- main.py
+++ main.py
@@ -4,23 +4,21 @@ from time import sleep # noqa
 
 print("File opened...")
 if __name__ == "__main__":
-    print("Main function started...")
+	print("Main function started...")
 
-    dhandler = DataHandler()
-    dDrawer = DataDrawer()
+	dhandler = DataHandler()
+	print('handler created')
+	dDrawer = DataDrawer()
+	print('drawer created')
+	try:
+		while True:
+			curr_data = dhandler.fetch_all(cache=False)
+			#just save matplotlib plots
+			print(curr_data)
+			dDrawer.draw_weather(curr_data)
+			sleep(60)
 
-    while True:
-        try:
-            curr_data = dhandler.fetch_all(cache=False)
-            #just save matplotlib plots
-            print(curr_data)
-            dDrawer.draw_weather(curr_data)
-        except KeyboardInterrupt:    
-            dDrawer.epd.Clear()
-            dDrawer.epd.epdconfig.module_exit(cleanup=True)
-            exit()
-        except Exception as e:
-            print(e)
-            pass
-        sleep(60)
-
+	except KeyboardInterrupt:
+		dDrawer.epd.Clear()
+		dDrawer.epd.epdconfig.module_exit(cleanup=True)
+		sleep(1)
